56548cc09025a0e94185d96646b9472e4a73e19c
Mikita Shmyhaliou <nszmygalow@gmail.com>
2024-01-30 15:02:09 +0100

    configure ec2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,10 +1,12 @@
 variable "sg_id" {}
- 
+
 resource "aws_instance" "webserver" {
     ami = "ami-0a3c3a20c09d6f377"
     instance_type = "t2.micro"
+    
     tags = {
-    name = "webserver"
+    Name = "webserver"
     }
     vpc_security_group_ids = [var.sg_id]
+    key_name = "web-key-pair"
 }
