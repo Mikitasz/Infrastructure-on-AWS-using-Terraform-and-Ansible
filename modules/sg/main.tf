d6b4d4d995f181f01ac5c7e638570b0324987c67
Mikita Shmyhaliou <nszmygalow@gmail.com>
2024-01-30 15:02:27 +0100

    configure sg

--- modules/sg/main.tf
+++ modules/sg/main.tf
@@ -1,12 +1,17 @@
-resource "aws_security_group" "allow_http" {
-  name        = "allow_http"
-  description = "Allow all inbound traffic to http"
+data "http" "myip" {
+  url = "https://ipv4.icanhazip.com"
+}
+
+
+resource "aws_security_group" "bastion-sg" {
+  name        = "bastion-sg"
+  description = "Allow ssh to bastion host"
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   }
 
 egress {
@@ -16,6 +21,67 @@ egress {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
-output "sg_id" {
-  value = aws_security_group.allow_http.id
+output "sg_bastion_id" {
+  value = aws_security_group.bastion-sg.id
 }
+
+
+resource "aws_security_group" "webserver-sg" {
+  name        = "webserver-sg"
+  description = "Allow all inbound traffic to http web server"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+   ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    security_groups = [aws_security_group.bastion-sg.id]
+   
+  }
+  
+  egress {
+      protocol    = -1
+      from_port   = 0
+      to_port     = 0
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+output "sg_web_id" {
+  value = aws_security_group.webserver-sg.id
+}
+
+
+resource "aws_security_group" "nginx-lb-sg" {
+  name        = "nginx-lb-sg"
+  description = "Nginx load balancer"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    #security_groups = [aws_asecurity_group.bastion-sg.id]
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  egress {
+      protocol    = -1
+      from_port   = 0
+      to_port     = 0
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+output "sg_nginx_id" {
+  value = aws_security_group.nginx-lb-sg.id
+}
+
+
