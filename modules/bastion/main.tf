1ba43e23c3df7aae36d2cd5b00635b59d09ba0a1
Mikita Shmyhaliou <nszmygalow@gmail.com>
2024-01-30 15:01:57 +0100

    configure bastion

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -1,9 +1,11 @@
-
+variable "sg_bastion_id" {}
  
 resource "aws_instance" "bastion" {
     ami = "ami-0a3c3a20c09d6f377"
     instance_type = "t2.micro"
     tags = {
-    name = "bastion"
+    Name = "bastion"
     }
+    vpc_security_group_ids = [var.sg_bastion_id]
+    key_name = "bastion-key-pair"
 }
